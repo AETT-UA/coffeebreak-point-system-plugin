cda1fc1ca1e7cb728f259bcb502253d99925be3b
vcnt <thiago.vicente@ua.pt>
2025-08-28 21:41:46 +0100

    Add QrCode component parameters

--- schemas/qrcode_component.py
+++ schemas/qrcode_component.py
@@ -1,7 +1,9 @@
 from schemas.ui.page import BaseComponentSchema
+from pydantic import Field
 
 class QrCode(BaseComponentSchema):
     """
     Schema for QR Code component.
     """
-    pass
+    message: str = Field(..., description="Message to show bellow the QrCode.")
+    alt_text: str = Field(..., description="Alt text for the QR code image.")
