4f0ef63724b26ac119dc55f3cbb939e72b6a5cb3
guilherme096 <guilherme.rosa60@gmail.com>
2026-02-28 19:03:28 +0000

    merge

--- schemas/staffpage.py
+++ schemas/staffpage.py
@@ -1,7 +1,6 @@
 from pydantic import Field
 from coffeebreak.schemas import BaseComponent as BaseComponentSchema
 
-<<<<<<< HEAD
 
 class StaffPage(BaseComponentSchema):
     """
@@ -9,13 +8,3 @@ class StaffPage(BaseComponentSchema):
     """
 
     title: str = Field(..., description="Title shown on the staff scanner page")
-=======
-class StaffPage(BaseComponentSchema):
-    """
-    Schema for Schedule component using FullCalendar
-
-    This component renders a configurable calendar focused on shorter timeframes
-    like days and weeks rather than months.
-    """
-    title: str = Field(..., description="Title of the schedule")
->>>>>>> 3074779d6b7bfbaff6e5344cc7e2a6c797820e86
